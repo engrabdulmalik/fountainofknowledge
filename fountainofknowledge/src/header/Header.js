eb5f5c2984e0a9e96879b8e8844152c8969121c3
engrabdulmalik <engrabdulmaliq@gmail.com>
2023-09-07 16:55:44 +0500

    new files

---
+++ fountainofknowledge/src/header/Header.js
@@ -0,0 +1,28 @@
+import "../header/Header.css";
+const Header = () => {
+  return (
+    <div className="main">
+      <div className="header1-1">
+        <div>
+          <p>Company Name</p>
+        </div>
+        <div>
+          <p>Address</p>
+        </div>
+      </div>
+      <div className="header1-2">
+        <div>
+          <img></img>
+        </div>
+        <div>
+          <h1>Company Name</h1>
+        </div>
+      </div>
+      <div className="nav">
+       
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ fountainofknowledge/src/homepage/Homepage.js
@@ -0,0 +1,10 @@
+import Header from "../header/Header";
+const Homepage = () => {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+};
+
+export default Homepage;
